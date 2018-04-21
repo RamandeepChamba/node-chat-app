10c109331b5987a9701ff0a23bc08b4c1fbd0797
Raman <rajpreet287@gmail.com>
2018-04-21 11:32:58 +0530

    Added form for messages and incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,25 @@
-const socket = io();
+var socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to server');
 
 	socket.on('newMessage', function (message) {
-		console.log('newMessage', message);
+		var li = $('<li></li>');
+		li.text(`${message.from}: ${message.text}`);
+
+		$('#messages').append(li);
 	});
 });
 
 socket.on('disconnect', function () {
 	console.log('Disconnected from server');
+});
+
+$('#message-form').on('submit', function(e) {
+	e.preventDefault();
+
+	var message = $('[name=message]').val();
+	socket.emit('createMessage', { from: 'User', text: message }, function() {
+
+	});
 });
